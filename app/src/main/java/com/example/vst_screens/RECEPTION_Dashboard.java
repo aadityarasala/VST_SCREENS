c561aed7a168db5b9b8616e197d3b8c2a29b16c3
Aaditya Rasala <aadityarasala19@gnu.ac.in>
2022-04-16 13:59:42 +0530

    frontend screen pushed to VST

---
+++ app/src/main/java/com/example/vst_screens/RECEPTION_Dashboard.java
@@ -0,0 +1,14 @@
+package com.example.vst_screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RECEPTION_Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reception_dashboard);
+    }
+}
---
+++ app/src/main/java/com/example/vst_screens/doctor_appointment.java
@@ -0,0 +1,14 @@
+package com.example.vst_screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class doctor_appointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_appointment);
+    }
+}
