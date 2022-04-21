c8a6a2278f7c640919855080e0beea8dc07e1718
Aaditya Rasala <aadityarasala19@gnu.ac.in>
2022-04-21 10:10:15 +0530

    developing notification

--- app/src/main/java/com/example/vst_screens/Notification.java
+++ app/src/main/java/com/example/vst_screens/Notification.java
@@ -1,16 +1,13 @@
 package com.example.vst_screens;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
 
-import android.app.NotificationManager;
+import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class Notification extends AppCompatActivity {
 
@@ -19,52 +16,22 @@ public class Notification extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
 
-        MyTimerTask myTask = new MyTimerTask();
-        Timer myTimer = new Timer();
-
-        myTimer.schedule(myTask, 5000, 1500);
-
     }
+}
+    private AlarmManager alarmMgr;
+    private PendingIntent alarmIntent;
 
-    class MyTimerTask extends TimerTask {
-        public void run() {
+    private void setAlert(int hour, int minute) {
+        alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
 
-            generateNotification(getApplicationContext(), "Hello");
-        }
-    }
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
 
-    private void generateNotification(Context context, String message) {
-
-        int icon = R.drawable.ic_launcher;
-        long when = System.currentTimeMillis();
-        String appname = context.getResources().getString(R.string.app_name);
-        NotificationManager notificationManager = (NotificationManager) context
-                .getSystemService(Context.NOTIFICATION_SERVICE);
-        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
-        Notification notification;
-        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
-                new Intent(context, MainActivity.class), 0);
-
-        // To support 2.3 os, we use "Notification" class and 3.0+ os will use
-        // "NotificationCompat.Builder" class.
-        if (currentapiVersion < android.os.Build.VERSION_CODES.HONEYCOMB) {
-            notification = new Notification(icon, message, 0);
-            notification.setLatestEventInfo(context, appname, message,
-                    contentIntent);
-            notification.flags = Notification.FLAG_AUTO_CANCEL;
-            notificationManager.notify((int) when, notification);
-
-        } else {
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(
-                    context);
-            notification = builder.setContentIntent(contentIntent)
-                    .setSmallIcon(icon).setTicker(appname).setWhen(0)
-                    .setAutoCancel(true).setContentTitle(appname)
-                    .setContentText(message).build();
-
-            notificationManager.notify((int) when, notification);
-
-        }
-
+        alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                1000 * 60 * 20, alarmIntent);
     }
 }
