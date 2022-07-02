dd16ea3e311de3a0f34089fce70b2c23bb9fc80a
Aaditya Rasala <aadityarasala19@gnu.ac.in>
2022-07-02 13:46:31 +0530

    spinner_MEDICINE

--- app/src/main/java/com/example/vst_screens/medicine_details_page.java
+++ app/src/main/java/com/example/vst_screens/medicine_details_page.java
@@ -3,12 +3,17 @@ package com.example.vst_screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class medicine_details_page extends AppCompatActivity {
+
+    private Spinner spinner_MEDICINE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medicine_details_page);
+
+        spinner_MEDICINE = findViewById(R.id.spinner_MEDICINE);
     }
 }
