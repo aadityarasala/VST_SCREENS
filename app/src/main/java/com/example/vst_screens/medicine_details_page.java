213697962031fea27322704404554d1717a0195b
Aaditya Rasala <aadityarasala19@gnu.ac.in>
2022-07-02 13:01:47 +0530

    medicine_details_page

---
+++ app/src/main/java/com/example/vst_screens/medicine_details_page.java
@@ -0,0 +1,14 @@
+package com.example.vst_screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class medicine_details_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicine_details_page);
+    }
+}
